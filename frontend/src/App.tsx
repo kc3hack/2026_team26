e86ed39c8f0106729eb092b926983502d3fd9fe1
oldsheeep3 <js2oyb@gmail.com>
2026-02-16 14:26:41 +0000

    [update] フロントの微修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import viteLogo from 'vite.svg';
 import './App.css';
 import reactLogo from './assets/react.svg';
 
@@ -9,10 +8,7 @@ function App() {
   return (
     <>
       <div>
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -23,7 +19,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">Click on the React logo to learn more</p>
     </>
   );
 }
